a6f6001217e42001c01270f2f3b633a6ae765b00
Bill Pfeiffer <pfeiffer.bill@gmail.com>
2017-02-12 22:33:37 -0500

    Demo Working with Objects

---
+++ Demo-workingiwthobjects.ps1
@@ -0,0 +1,43 @@
+#credit to Jeff Hicks from Pluralsight - This is for me to help gain muscle memory
+
+#demo object members in action
+
+#region Get-Member
+
+get-service
+get-service | get-member
+
+#see just properties
+get-service | get-member -Membertype Properties
+
+#once you know the properties you can use them
+get-service | Select Name,Status, ServiceType | ft -AutoSize
+
+#end region
+
+#region creating custom properties
+#select object#start some new processes
+notepad;calc.exe
+
+#start time only works locally
+get-process | where starttime | select ID,Name,StartTime, @{Name="RunTime";Expression={(Get-Date) - $_.StartTime}} | sort RunTime
+
+#format-table
+$computers = $env:COMPUTERNAME
+Get-CimInstance win32_logicaldisk -filter "drivetype=3" -comp $computers | Format-Table -Groupby PSComputername -Property DeviceID,VolumeName,
+@{N="SizeGB";E={[math]::Round($_.Size/1GB)}},
+@{N="FreeGB";E={[math]::Round($_.Freespace/1GB,2)}},
+@{N="PerFree";E={[math]::Round(($_.Freespace/$_.size)*100,2)}}
+
+#passthru a single change
+dir C:\scripts -file -recurse
+Add-Member AliasProperty Size -value length -PassThru | saort size -descending | select FullName,size -first 10
+
+#a practical example
+$files | Add-Member –Membertype AliasProperty -Name Size –Value Length 
+$files | Add-Member ScriptProperty –Name FileAge –Value {(Get-Date) - ($this.LastWriteTime)} 
+$files | Add-Member ScriptProperty –Name FileAgeDays –Value {[int]((Get-Date) - ($this.LastWriteTime)).TotalDays} 
+$files | Add-Member Noteproperty Computername $env:computername 
+$files | sort Size –desc | Select FullName,Size,FileAge*,Computername –first 10
+
+#endregion
