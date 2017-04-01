49c588ae596a2c6cb5c6ade30d04bbe68dea99b2
Bill Pfeiffer <pfeiffer.bill@gmail.com>
2017-03-31 22:27:07 -0400

    Formatting demo

--- Demo-PowershellFormatting.ps1
+++ Demo-PowershellFormatting.ps1
@@ -28,9 +28,45 @@ get-eventlog System -newest 50 | Format-Table -GroupBy EntryType -Property TimeG
 #should sort first
 get-eventlog System -newest 50 | Sort EntryType | Format-Table -GroupBy EntryType -Property TimeGenerated,Source,Message -Wrap
 
+#formatting has to be last
+
+#this will fail
+$computer = Get-ChildItem Env:\COMPUTERNAME | select Value | fw
+
+Get-CimInstance Win32_OperatingSystem -comp $computer | Format-Table @{n="OS";e={
+    [regex]$rx="Microsoft|Windows|\(R|)"
+    $rx.Replace($_.Caption,""),Trim()}
+    },InstallDate,
+    @{n="Computername";e={$_.CSName}},
+    @{n="SP";e={$_.ServicePackmajorversion}} -auto |
+    Sort InstallDate
+}}
+
+#see why?
+gcim win32_operatingsystem | ft Caption,CSName,InstallDate | gm
+
+#process data before formatting
+Get-CimInstance Win32_OperatingSystem -comp $computer | Format-Table @{n="OS";e={
+    [regex]$rx="Microsoft|Windows|\(R|)"
+    $rx.Replace($_.Caption,"").Trim()}
+    },InstallDate,
+    @{n="Computername";e={$_.CSName}},
+    @{n="SP";e={$_.ServicePackmajorversion}} -auto |
+    Sort InstallDate
+
 #endregion
 
 #region Format-List
+#great way to discover object properties
+get-process system | Format-List *
+get-process | ft
+get-process | fl
+dir C:\scripts
+dir C:\scripts | fl
+
+#grouping
+dir C:\scripts |sort Extension
+dir C:\scripts |sort Extension | fl
 
 
 
